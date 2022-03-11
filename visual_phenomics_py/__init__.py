eec83580d52f51519b420fb38e627eb8e1df5349
Sebastian <sebastian.kuhlgert@gmail.com>
2022-03-11 18:59:46 -0500

    change function name

--- visual_phenomics_py/__init__.py
+++ visual_phenomics_py/__init__.py
@@ -11,6 +11,7 @@ See :func:`~visual_phenomics_py.about.info`
 See :func:`~visual_phenomics_py.about.samples`
 See :func:`~visual_phenomics_py.about.description`
 See :func:`~visual_phenomics_py.plot.plot`
+See :func:`~visual_phenomics_py.plot.plot_light`
 
 See the online readme for more information: https://github.com/SeBassTian23/Visual-Phenomics-Python
 """
@@ -18,4 +19,4 @@ See the online readme for more information: https://github.com/SeBassTian23/Visu
 from visual_phenomics_py.buildframe import dataframe
 from visual_phenomics_py.calculate import calculate, calculate_additional, calculate_custom
 from visual_phenomics_py.about import info, samples, description
-from visual_phenomics_py.plot import plot_light, plot
+from visual_phenomics_py.plot import plot, plot_light
--- visual_phenomics_py/plot.py
+++ visual_phenomics_py/plot.py
@@ -5,60 +5,68 @@ Plot data from visual phenomics data
 from numpy import ceil
 import matplotlib.pyplot as plt
 
-def plot_light(df=None):
-  """
-  Plot light intensities 
 
-  :param df: DataFrame
-  :returns: Plot
-  """
-  if df is not None:
-    df.groupby(['time','light_intensity'])[['time','light_intensity']].agg('mean').plot( 
-      kind = 'scatter',
-      x = 'time',
-      y = 'light_intensity',
-      title = 'Light Intensities'
+def plot_light(df=None):
+    """Plot light intensities
+
+    Light intensities plotted over time in a single figure.
+
+    :param df: DataFrame
+    :returns: Plot
+    """
+
+    if df is None:
+        raise Exception('No DataFrame selected.')
+
+    df.groupby(['time', 'light_intensity'])[['time', 'light_intensity']].agg('mean').plot(
+        kind='scatter',
+        x='time',
+        y='light_intensity',
+        title='Light Intensities'
     )
-  else:
-    print('No DataFrame selected.')
 
 
 def plot(df=None, param=None, *, avg=False):
-  """
-  Plot a parameter, either for individual samples or as an average with standard-deviation
-  for each sample name.
-  Each day is represented in an individual plot.
+    """Plot a single parameter over time in separate figures for each day.
 
-  :param df: DataFrame
-  :param param: Fluorescence based parameter (e.g. phi2)
-  :param avg: average with standard-deviation (default: False)
-  :returns: Plot
-  """
-  days = int(ceil(df['time'].max()/24))
+    Plot a parameter, either for individual samples or as an average with standard-deviation
+    for each sample name. Each day is represented in an individual figure.
 
-  for i in range(0,days):
-    df_tmp = df[(df['time'].between(i * 24, i * 24 + 24))]
+    :param df: DataFrame
+    :param param: Fluorescence based parameter (e.g. phi2)
+    :param avg: average with standard-deviation (default: False)
+    :returns: Plot
+    """
 
-    fig, ax = plt.subplots( figsize=(8,5))
-    for strain in df['name'].unique():
-      if avg:
-        x = df_tmp[ (df_tmp['name'] == strain) ].groupby(['name','time'])['time']
-        y = df_tmp[ (df_tmp['name'] == strain) ].groupby(['name','time'])[param]
-    
-        ax.errorbar( x.agg('mean'), y.agg('mean'), 
-          yerr=y.agg('sem'), fmt='.-', markersize=10, capsize=4, 
-          elinewidth=1, label= strain )
-      else:
-        ax.scatter( 
-          df_tmp[df_tmp['name'] == strain]['time'],
-          df_tmp[df_tmp['name'] == strain][param],
-          label=strain
-        )
-        
-    ax.set_title( 'Day {0}'.format(i+1) )
-    ax.set_xlabel('Time [h]')
-    ax.set_ylabel(param)
-    plt.legend()
-    plt.tight_layout()
-    plt.show()
+    if df is None:
+        raise Exception('No DataFrame selected.')
 
+    days = int(ceil(df['time'].max()/24))
+
+    for i in range(0, days):
+        df_tmp = df[(df['time'].between(i * 24, i * 24 + 24))]
+
+        fig, ax = plt.subplots(figsize=(8, 5))
+        for strain in df['name'].unique():
+            if avg:
+                x = df_tmp[(df_tmp['name'] == strain)].groupby(
+                    ['name', 'time'])['time']
+                y = df_tmp[(df_tmp['name'] == strain)].groupby(
+                    ['name', 'time'])[param]
+
+                ax.errorbar(x.agg('mean'), y.agg('mean'),
+                            yerr=y.agg('sem'), fmt='.-', markersize=10, capsize=4,
+                            elinewidth=1, label=strain)
+            else:
+                ax.scatter(
+                    df_tmp[df_tmp['name'] == strain]['time'],
+                    df_tmp[df_tmp['name'] == strain][param],
+                    label=strain
+                )
+
+        ax.set_title('Day {0}'.format(i+1))
+        ax.set_xlabel('Time [h]')
+        ax.set_ylabel(param)
+        plt.legend()
+        plt.tight_layout()
+        plt.show()

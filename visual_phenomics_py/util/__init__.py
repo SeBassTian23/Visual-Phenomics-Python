3666250a544e640bdb7a6ab63020695d78b92fb5
Sebastian <sebastian.kuhlgert@gmail.com>
2022-05-27 10:45:58 -0400

    move parameter calculation into util module

---
+++ visual_phenomics_py/util/__init__.py
@@ -0,0 +1,9 @@
+"""Visual-Phenomics-Python
+
+This package allows you to calculate parameters from recorded fluorescence measurements.
+
+See the online readme for more information: https://github.com/SeBassTian23/Visual-Phenomics-Python
+"""
+
+from visual_phenomics_py.util.parameters import fvfm, npq, npqt, phi2, phino, phinot, phinpq, phinpqt, qe, qesv, qet, qi, qit, ql, qp
+from visual_phenomics_py.util.parameters_additional import lef, vx, sphi2, sphinpq

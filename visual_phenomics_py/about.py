b6ce5520a82afe431d3881ec62ef565ea3a349d5
Sebastian <sebastian.kuhlgert@gmail.com>
2022-06-02 17:05:44 -0400

    include folders into description

--- visual_phenomics_py/about.py
+++ visual_phenomics_py/about.py
@@ -41,7 +41,11 @@ def description(df=None):
     if 'light_intensity' in df:
         light = df['light_intensity'].max()
 
-    description = 'The current DataFrame contains {0} experiment(s) with {1} sample(s) of {2} individual lines. The duration of the experiment was {4} hours ({5} day(s)) with a maximum light intensity of {6} uE.\n\n# Lines: {3}\n\n# Experiments:\n{7}'.format(
+    folders = ""
+    if 'folder' in df:
+        folders = ", combined from {0} folder(s) [{1}]".format(len(df['folder'].unique()), ", ".join(df['folder'].unique().tolist()))
+
+    description = 'The current DataFrame contains {0} experiment(s) with {1} sample(s) of {2} individual lines{8}. The duration of the experiment was {4} hours ({5} day(s)) with a maximum light intensity of {6} uE.\n\n# Lines:\n{3}\n\n# Experiments:\n{7}'.format(
         len(df['experiment'].unique()),
         len(df['sample'].unique()),
         len(df['name'].unique()),
@@ -49,7 +53,8 @@ def description(df=None):
         df['time'].max(),
         (df['time'].max()/24),
         light,
-        "\n".join(
+        ", ".join(
             sorted(df['experiment'].unique().tolist(), key=str.casefold)),
+        folders
     )
     print(description)
